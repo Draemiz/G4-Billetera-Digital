f179aebd2261f50a83f12924bd77c4e77876d9d3
Draemiz <draemizgx1@gmail.com>
2026-03-26 20:06:32 -0500

    feat: creación de clase Billetera con atributos y constructor

---
+++ Billetera.java
@@ -0,0 +1,13 @@
+public class Billetera {
+    private String propietario;
+    private String numeroCuenta;
+    private double saldo;
+    private boolean activa;
+
+    public Billetera(String propietario, String numeroCuenta, double saldoInicial) {
+        this.propietario = propietario;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldoInicial;
+        this.activa = true;
+    }
+}
