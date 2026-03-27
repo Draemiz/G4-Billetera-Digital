1cf9c994e805e252f2d6c1c9af3fd7fe078c1135
beckerdelsal-pixel <beckerdelsal@gmail.com>
2026-03-26 19:55:22 -0500

    feat: * add constructor class, get and set methods

--- Contacto.java
+++ Contacto.java
@@ -5,5 +5,45 @@ public class Contacto {
     private String banco;
     private boolean favorito;
 
+    public Contacto(String nombre, String cuentaDestino, String banco) {
+        this.nombre = nombre;
+        this.cuentaDestino = cuentaDestino;
+        this.banco = banco;
+        favorito = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuentaDestino() {
+        return cuentaDestino;
+    }
+
+    public void setCuentaDestino(String cuentaDestino) {
+        this.cuentaDestino = cuentaDestino;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public boolean isFavorito() {
+        return favorito;
+    }
+
+    public void setFavorito(boolean favorito) {
+        this.favorito = favorito;
+    }
+
+    
    
 }
