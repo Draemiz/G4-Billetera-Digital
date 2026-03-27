63d8548ec90fdeed6b0106eaf30a802d5f1e9585
IsmHernandez11 <ismasnayhernandez2010@gmail.com>
2026-03-26 20:05:33 -0500

    cambie el nombre de transacciones a transaccion

---
+++ Transaccion.java
@@ -0,0 +1,29 @@
+//IsmaelHernandez
+public class transacciones {
+    //Atributos =)
+    private String tipo;
+    private double monto;
+    private String descripcion;
+    private boolean exitosa;
+
+    public Transaccion(String tipo, double monto, String descripcion) {
+        this.tipo = tipo;
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.exitosa = true;
+    }
+     public void mostrarDetalle() {
+        System.out.println("--- Detalle de Transacción ---");
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Monto: $" + monto);
+        System.out.println("Descripción: " + descripcion);
+        System.out.println("Estado: " + (exitosa ? "Exitosa" : "Fallida"));
+    }
+public void aplicarComision(double porcentaje) {
+        double comision = monto * (porcentaje / 100);
+        double total = monto + comision;
+
+        System.out.println("Comisión (" + porcentaje + "%): $" + String.format("%.2f", comision));
+        System.out.println("Monto total con comisión: $" + String.format("%.2f", total));
+    }
+}
--- transacciones.java
+++
@@ -1,4 +0,0 @@
-public class transacciones {
-    //Atributos
-    private String tipo;
-}
